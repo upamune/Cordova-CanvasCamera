3b6cba5353cd3d8a1a80cad7b574068ca024c58f
William P. Riley-Land <william@kun.io>
2014-03-18 13:09:26 -0500

    Add win/lose callbacks.

--- CanvasCamera/CanvasCamera.js
+++ CanvasCamera/CanvasCamera.js
@@ -6,10 +6,20 @@
 //
 //  MIT License
 
+function win (data) {
+  cordova.logger.log('won!')
+  cordova.logger.log(data)
+}
+
+function lose (error) {
+  cordova.logger.log('Camera error.');
+  cordova.logger.log(error);
+}
+
 CanvasCamera = {
   start: function(options) {
     // TODO: add support for options (fps, capture quality, capture format, etc.)
-    cordova.exec(false, false, "CanvasCamera", "startCapture", [""]);
+    cordova.exec(win, lose, "CanvasCamera", "startCapture", [""]);
   },
   capture: function(data) {
     // LAZYME: Override this function with something like...
